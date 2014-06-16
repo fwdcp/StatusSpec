ef792d7ada288bf4cb231e858a2561c048f46c13
thesupremecommander <thesupremecmdr@gmail.com>
2014-06-16 16:57:14 -0400

    Utilize SourceHook methods to manage hooks.

--- src/hooks.h
+++ src/hooks.h
@@ -39,6 +39,13 @@ class C_TFPlayer;
 #define OFFSET_DESTROYGLOWEFFECT 225
 #endif
 
+class StatusSpecUnloader: public SourceHook::Impl::UnloadListener
+{
+public:
+	virtual void ReadyToUnload(SourceHook::Plugin plug);
+};
+
+inline void StatusSpecUnloader::ReadyToUnload(SourceHook::Plugin plug) {};
 
 extern SourceHook::ISourceHook *g_SHPtr;
 extern int g_PLID;
