9910de19156585a054c8d08ce16ff1080a9c36f7
thesupremecommander <thesupremecmdr@gmail.com>
2014-07-15 17:13:25 -0400

    Add in adjusted versions of a game console from the Alien Swarm SDK.

---
+++ src/vgui_controls/gameconsoledialog.h
@@ -0,0 +1,21 @@
+/*
+*  gameconsoledialog.cpp
+*  StatusSpec project
+*  Derived from the Alien Swarm SDK
+*
+*/
+
+#pragma once
+
+#include "vgui_controls/consoledialog.h"
+
+class CGameConsoleDialog : public vgui::CConsoleDialog
+{
+	DECLARE_CLASS_SIMPLE(CGameConsoleDialog, vgui::CConsoleDialog);
+
+public:
+	CGameConsoleDialog();
+
+private:
+	MESSAGE_FUNC_CHARPTR(OnCommandSubmitted, "CommandSubmitted", command);
+};
