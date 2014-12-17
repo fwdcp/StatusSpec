4d01fd964775f9806ea8a8c7f231624cd6950a3e
thesupremecommander <thesupremecmdr@gmail.com>
2014-12-16 19:50:38 -0500

    Add missing exception.

--- src/exceptions.h
+++ src/exceptions.h
@@ -19,6 +19,28 @@
 #define noexcept _NOEXCEPT
 #endif
 
+class module_not_loaded : public std::exception {
+public:
+	module_not_loaded(const char *name) noexcept;
+	virtual const char *what() const noexcept;
+private:
+	const char *moduleName;
+};
+
+inline module_not_loaded::module_not_loaded(const char *name) noexcept{
+	moduleName = name;
+}
+
+inline const char *module_not_loaded::what() const noexcept{
+	std::string s;
+	std::stringstream ss;
+
+	ss << "Module " << moduleName << " not loaded!\n";
+	ss >> s;
+
+	return s.c_str();
+}
+
 class bad_pointer : public std::exception {
 public:
 	bad_pointer(const char *type) noexcept;
