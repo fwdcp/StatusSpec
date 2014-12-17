627ce4d1d8ea19fa0a310de2b484ad9385f54982
thesupremecommander <thesupremecmdr@gmail.com>
2014-12-17 00:29:50 -0500

    Add a message when unloading a module.

--- src/modules.cpp
+++ src/modules.cpp
@@ -16,6 +16,9 @@
 
 void ModuleManager::UnloadAllModules() {
 	for (auto iterator = modules.begin(); iterator != modules.end(); ++iterator) {
+		PRINT_TAG();
+		ConColorMsg(Color(0, 255, 0, 255), "Module %s unloaded!\n", iterator->first.c_str());
+
 		delete iterator->second;
 	}
 
