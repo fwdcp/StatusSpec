5b12e1d756ee8c1066f7efc4f4ba848176b7c546
thesupremecommander <thesupremecmdr@gmail.com>
2014-12-19 15:40:22 -0500

    Migrate player outlines module.

--- src/modules/playeroutlines.h
+++ src/modules/playeroutlines.h
@@ -24,6 +24,7 @@
 #include "../funcs.h"
 #include "../glows.h"
 #include "../ifaces.h"
+#include "../modules.h"
 #include "../player.h"
 #include "../tfdefs.h"
 
@@ -31,9 +32,11 @@
 #define strtoull _strtoui64
 #endif
 
-class PlayerOutlines {
+class PlayerOutlines : public Module {
 public:
-	PlayerOutlines();
+	PlayerOutlines(std::string name);
+
+	static bool CheckDependencies(std::string name);
 
 	bool DoPostScreenSpaceEffectsHook(const CViewSetup *pSetup);
 	void FrameHook(ClientFrameStage_t curStage);
@@ -41,7 +44,8 @@ private:
 	std::map<std::string, ColorConCommand_t> colors;
 	int doPostScreenSpaceEffectsHook;
 	int frameHook;
-	std::map<EHANDLE, CGlowObject *> glows;
+	std::map<EHANDLE, GlowManager::GlowObject *> glows;
+	GlowManager glowManager;
 
 	Color GetGlowColor(Player player);
 	void SetGlowEffect(IClientEntity *entity, bool enabled, Vector color = Vector(1.0f, 1.0f, 1.0f), float alpha = 1.0f);
@@ -56,5 +60,3 @@ private:
 	void ToggleEnabled(IConVar *var, const char *pOldValue, float flOldValue);
 	void ToggleFade(IConVar *var, const char *pOldValue, float flOldValue);
 };
-
-extern PlayerOutlines *g_PlayerOutlines;
--- src/statusspec.cpp
+++ src/statusspec.cpp
@@ -40,6 +40,7 @@ bool StatusSpecPlugin::Load(CreateInterfaceFn interfaceFactory, CreateInterfaceF
 	g_ModuleManager->LoadModule<Killstreaks>("Killstreaks");
 	g_ModuleManager->LoadModule<LocalPlayer>("Local Player");
 	g_ModuleManager->LoadModule<MedigunInfo>("Medigun Info");
+	g_ModuleManager->LoadModule<PlayerOutlines>("Player Outlines");
 	
 	ConVar_Register();
 
