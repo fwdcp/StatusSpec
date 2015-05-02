0898ba999337cb02e077a4938a9ec714c91e460e
thesupremecommander <thesupremecmdr@gmail.com>
2015-05-01 20:18:26 -0400

    Add logging to file functionality.

--- src/modules/consolemanager.h
+++ src/modules/consolemanager.h
@@ -25,14 +25,22 @@ public:
 	static bool CheckDependencies(std::string name);
 private:
 	void ConsoleColorPrintfHook(const Color &clr, const char *message);
+	void ConsoleColorPrintfOverride(const Color &clr, const char *message);
 	void ConsoleDPrintfHook(const char *message);
+	void ConsoleDPrintfOverride(const char *message);
 	void ConsolePrintfHook(const char *message);
+	void ConsolePrintfOverride(const char *message);
 
 	bool CheckFilters(std::string message);
+	void LogMessage(std::string message);
 
-	int consoleColorPrintfHook;
-	int consoleDPrintfHook;
-	int consolePrintfHook;
+	int consoleColorPrintfPostHook;
+	int consoleColorPrintfPreHook;
+	int consoleDPrintfPostHook;
+	int consoleDPrintfPreHook;
+	int consolePrintfPostHook;
+	int consolePrintfPreHook;
+	std::string currentLogFile;
 	std::set<std::string> filters;
 
 	ConCommand *filter_add;
@@ -40,9 +48,11 @@ private:
 	ConCommand *filter_remove;
 	ConCommand *flags_add;
 	ConCommand *flags_remove;
+	ConVar *logging_enabled;
 	void AddFilter(const CCommand &command);
 	void AddFlags(const CCommand &command);
 	void RemoveFilter(const CCommand &command);
 	void RemoveFlags(const CCommand &command);
 	void ToggleFilterEnabled(IConVar *var, const char *pOldValue, float flOldValue);
+	void ToggleLoggingEnabled(IConVar *var, const char *pOldValue, float flOldValue);
 };
