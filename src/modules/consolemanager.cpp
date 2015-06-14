8357b52e352732a02144c6e5a980831ab3e50bb9
thesupremecommander <thesupremecmdr@gmail.com>
2015-06-14 17:06:35 -0400

    Remove console logging.

--- src/modules/consolemanager.cpp
+++ src/modules/consolemanager.cpp
@@ -13,12 +13,9 @@
 #include <regex>
 
 #include "convar.h"
-#include "filesystem.h"
 #include "icvar.h"
-#include "utlbuffer.h"
 
 #include "../funcs.h"
-#include "../ifaces.h"
 
 // this is a total hijack of a friend class declared but never defined in the public SDK
 class CCvar {
@@ -36,30 +33,19 @@ void CCvar::SetFlags(ConCommandBase *base, int flags) {
 }
 
 ConsoleManager::ConsoleManager(std::string name) : Module(name) {
-	consoleColorPrintfPostHook = 0;
-	consoleColorPrintfPreHook = 0;
-	consoleDPrintfPostHook = 0;
-	consoleDPrintfPreHook = 0;
-	consolePrintfPostHook = 0;
-	consolePrintfPreHook = 0;
+	consoleColorPrintfHook = 0;
+	consoleDPrintfHook = 0;
+	consolePrintfHook = 0;
 
 	filter_add = new ConCommand("statusspec_consolemanager_filter_add", [](const CCommand &command) { g_ModuleManager->GetModule<ConsoleManager>("Console Manager")->AddFilter(command); }, "add a console filter", FCVAR_NONE);
 	filter_enabled = new ConVar("statusspec_consolemanager_filter_enabled", "0", FCVAR_NONE, "enable console filtering", [](IConVar *var, const char *pOldValue, float flOldValue) { g_ModuleManager->GetModule<ConsoleManager>("Console Manager")->ToggleFilterEnabled(var, pOldValue, flOldValue); });
 	filter_remove = new ConCommand("statusspec_consolemanager_filter_remove", [](const CCommand &command) { g_ModuleManager->GetModule<ConsoleManager>("Console Manager")->RemoveFilter(command); }, "remove a console filter", FCVAR_NONE);
 	flags_add = new ConCommand("statusspec_consolemanager_flags_add", [](const CCommand &command) { g_ModuleManager->GetModule<ConsoleManager>("Console Manager")->AddFlags(command); }, "add a flag to a console command or variable", FCVAR_NONE);
 	flags_remove = new ConCommand("statusspec_consolemanager_flags_remove", [](const CCommand &command) { g_ModuleManager->GetModule<ConsoleManager>("Console Manager")->RemoveFlags(command); }, "remove a flag from a console command or variable", FCVAR_NONE);
-	logging_enabled = new ConVar("statusspec_consolemanager_logging_enabled", "0", FCVAR_NONE, "enable console logging to file", [](IConVar *var, const char *pOldValue, float flOldValue) { g_ModuleManager->GetModule<ConsoleManager>("Console Manager")->ToggleLoggingEnabled(var, pOldValue, flOldValue); });
 }
 
 bool ConsoleManager::CheckDependencies(std::string name) {
 	bool ready = true;
-
-	if (!Interfaces::pFileSystem) {
-		PRINT_TAG();
-		Warning("Required interface IFileSystem for module %s not available!\n", name.c_str());
-
-		ready = false;
-	}
 
 	if (!g_pCVar) {
 		PRINT_TAG();
@@ -72,12 +58,6 @@ bool ConsoleManager::CheckDependencies(std::string name) {
 }
 
 void ConsoleManager::ConsoleColorPrintfHook(const Color &clr, const char *message) {
-	LogMessage(message);
-
-	RETURN_META(MRES_IGNORED);
-}
-
-void ConsoleManager::ConsoleColorPrintfOverride(const Color &clr, const char *message) {
 	if (CheckFilters(message)) {
 		RETURN_META(MRES_SUPERCEDE);
 	}
@@ -86,12 +66,6 @@ void ConsoleManager::ConsoleColorPrintfOverride(const Color &clr, const char *me
 }
 
 void ConsoleManager::ConsoleDPrintfHook(const char *message) {
-	LogMessage(message);
-
-	RETURN_META(MRES_IGNORED);
-}
-
-void ConsoleManager::ConsoleDPrintfOverride(const char *message) {
 	if (CheckFilters(message)) {
 		RETURN_META(MRES_SUPERCEDE);
 	}
@@ -100,12 +74,6 @@ void ConsoleManager::ConsoleDPrintfOverride(const char *message) {
 }
 
 void ConsoleManager::ConsolePrintfHook(const char *message) {
-	LogMessage(message);
-
-	RETURN_META(MRES_IGNORED);
-}
-
-void ConsoleManager::ConsolePrintfOverride(const char *message) {
 	if (CheckFilters(message)) {
 		RETURN_META(MRES_SUPERCEDE);
 	}
@@ -121,18 +89,6 @@ bool ConsoleManager::CheckFilters(std::string message) {
 	}
 
 	return false;
-}
-
-void ConsoleManager::LogMessage(std::string message) {
-	CUtlBuffer buffer;
-	buffer.PutString(message.c_str());
-	size_t length = buffer.TellPut();
-
-	void *memory = malloc(length);
-	buffer = CUtlBuffer(memory, length);
-	buffer.PutString(message.c_str());
-
-	FSAsyncStatus_t status = Interfaces::pFileSystem->AsyncAppend(currentLogFile.c_str(), buffer.Base(), buffer.TellPut(), true);
 }
 
 void ConsoleManager::AddFlags(const CCommand &command) {
@@ -287,78 +243,34 @@ void ConsoleManager::RemoveFilter(const CCommand &command) {
 
 void ConsoleManager::ToggleFilterEnabled(IConVar *var, const char *pOldValue, float flOldValue) {
 	if (filter_enabled->GetBool()) {
-		if (!consoleColorPrintfPreHook) {
-			consoleColorPrintfPreHook = Funcs::AddHook_ICvar_ConsoleColorPrintf(g_pCVar, SH_MEMBER(this, &ConsoleManager::ConsoleColorPrintfOverride), false);
+		if (!consoleColorPrintfHook) {
+			consoleColorPrintfHook = Funcs::AddHook_ICvar_ConsoleColorPrintf(g_pCVar, SH_MEMBER(this, &ConsoleManager::ConsoleColorPrintfHook), false);
 		}
 
-		if (!consoleDPrintfPreHook) {
-			consoleDPrintfPreHook = Funcs::AddHook_ICvar_ConsoleDPrintf(g_pCVar, SH_MEMBER(this, &ConsoleManager::ConsoleDPrintfOverride), false);
+		if (!consoleDPrintfHook) {
+			consoleDPrintfHook = Funcs::AddHook_ICvar_ConsoleDPrintf(g_pCVar, SH_MEMBER(this, &ConsoleManager::ConsoleDPrintfHook), false);
 		}
 		
-		if (!consolePrintfPreHook) {
-			consolePrintfPreHook = Funcs::AddHook_ICvar_ConsolePrintf(g_pCVar, SH_MEMBER(this, &ConsoleManager::ConsolePrintfOverride), false);
+		if (!consolePrintfHook) {
+			consolePrintfHook = Funcs::AddHook_ICvar_ConsolePrintf(g_pCVar, SH_MEMBER(this, &ConsoleManager::ConsolePrintfHook), false);
 		}
 	}
 	else {
-		if (consoleColorPrintfPreHook) {
-			if (Funcs::RemoveHook(consoleColorPrintfPreHook)) {
-				consoleColorPrintfPreHook = 0;
+		if (consoleColorPrintfHook) {
+			if (Funcs::RemoveHook(consoleColorPrintfHook)) {
+				consoleColorPrintfHook = 0;
 			}
 		}
 
-		if (consoleDPrintfPreHook) {
-			if (Funcs::RemoveHook(consoleDPrintfPreHook)) {
-				consoleDPrintfPreHook = 0;
+		if (consoleDPrintfHook) {
+			if (Funcs::RemoveHook(consoleDPrintfHook)) {
+				consoleDPrintfHook = 0;
 			}
 		}
 
-		if (consolePrintfPreHook) {
-			if (Funcs::RemoveHook(consolePrintfPreHook)) {
-				consolePrintfPreHook = 0;
-			}
-		}
-	}
-}
-
-void ConsoleManager::ToggleLoggingEnabled(IConVar *var, const char *pOldValue, float flOldValue) {
-	if (logging_enabled->GetBool()) {
-		if (!consoleColorPrintfPostHook) {
-			consoleColorPrintfPostHook = Funcs::AddHook_ICvar_ConsoleColorPrintf(g_pCVar, SH_MEMBER(this, &ConsoleManager::ConsoleColorPrintfHook), true);
-		}
-
-		if (!consoleDPrintfPostHook) {
-			consoleDPrintfPostHook = Funcs::AddHook_ICvar_ConsoleDPrintf(g_pCVar, SH_MEMBER(this, &ConsoleManager::ConsoleDPrintfHook), true);
-		}
-
-		if (!consolePrintfPostHook) {
-			consolePrintfPostHook = Funcs::AddHook_ICvar_ConsolePrintf(g_pCVar, SH_MEMBER(this, &ConsoleManager::ConsolePrintfHook), true);
-		}
-
-		Interfaces::pFileSystem->CreateDirHierarchy("logs", "MOD");
-
-		char logName[64];
-		time_t currentTime = time(nullptr);
-		struct tm *currentLocalTime = localtime(&currentTime);
-		strftime(logName, sizeof(logName), "logs/clientconsole-%Y-%m-%d-%H-%M-%S.log", currentLocalTime);
-
-		currentLogFile = std::string(logName);
-	}
-	else {
-		if (consoleColorPrintfPostHook) {
-			if (Funcs::RemoveHook(consoleColorPrintfPostHook)) {
-				consoleColorPrintfPostHook = 0;
-			}
-		}
-
-		if (consoleDPrintfPostHook) {
-			if (Funcs::RemoveHook(consoleDPrintfPostHook)) {
-				consoleDPrintfPostHook = 0;
-			}
-		}
-
-		if (consolePrintfPostHook) {
-			if (Funcs::RemoveHook(consolePrintfPostHook)) {
-				consolePrintfPostHook = 0;
+		if (consolePrintfHook) {
+			if (Funcs::RemoveHook(consolePrintfHook)) {
+				consolePrintfHook = 0;
 			}
 		}
 	}
--- src/modules/consolemanager.h
+++ src/modules/consolemanager.h
@@ -25,22 +25,14 @@ public:
 	static bool CheckDependencies(std::string name);
 private:
 	void ConsoleColorPrintfHook(const Color &clr, const char *message);
-	void ConsoleColorPrintfOverride(const Color &clr, const char *message);
 	void ConsoleDPrintfHook(const char *message);
-	void ConsoleDPrintfOverride(const char *message);
 	void ConsolePrintfHook(const char *message);
-	void ConsolePrintfOverride(const char *message);
 
 	bool CheckFilters(std::string message);
-	void LogMessage(std::string message);
 
-	int consoleColorPrintfPostHook;
-	int consoleColorPrintfPreHook;
-	int consoleDPrintfPostHook;
-	int consoleDPrintfPreHook;
-	int consolePrintfPostHook;
-	int consolePrintfPreHook;
-	std::string currentLogFile;
+	int consoleColorPrintfHook;
+	int consoleDPrintfHook;
+	int consolePrintfHook;
 	std::set<std::string> filters;
 
 	ConCommand *filter_add;
@@ -48,11 +40,9 @@ private:
 	ConCommand *filter_remove;
 	ConCommand *flags_add;
 	ConCommand *flags_remove;
-	ConVar *logging_enabled;
 	void AddFilter(const CCommand &command);
 	void AddFlags(const CCommand &command);
 	void RemoveFilter(const CCommand &command);
 	void RemoveFlags(const CCommand &command);
 	void ToggleFilterEnabled(IConVar *var, const char *pOldValue, float flOldValue);
-	void ToggleLoggingEnabled(IConVar *var, const char *pOldValue, float flOldValue);
 };
