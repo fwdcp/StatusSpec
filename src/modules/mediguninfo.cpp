38f5ee1e22ddd20a21000f67901ddbbf68e7d8a5
thesupremecommander <thesupremecmdr@gmail.com>
2015-06-20 12:52:08 -0400

    Finish new medigun panels.

--- src/modules/mediguninfo.cpp
+++ src/modules/mediguninfo.cpp
@@ -15,6 +15,7 @@
 #include "cbase.h"
 #include "convar.h"
 #include "iclientmode.h"
+#include "vgui/IScheme.h"
 #include "vgui/ISurface.h"
 #include "vgui/IVGui.h"
 #include "vgui_controls/EditablePanel.h"
@@ -26,11 +27,15 @@
 #include "../player.h"
 
 class MedigunInfo::MainPanel : public vgui::EditablePanel {
+	DECLARE_CLASS_SIMPLE(MainPanel, vgui::EditablePanel);
+
 public:
 	MainPanel(vgui::Panel *parent, const char *panelName);
 
 	virtual void ApplySettings(KeyValues *inResourceData);
+	virtual void LoadControlSettings(const char *dialogResourceName, const char *pathID = NULL, KeyValues *pPreloadedKeyValues = NULL, KeyValues *pConditions = NULL);
 	virtual void OnTick();
+
 private:
 	int bluBaseX;
 	int bluBaseY;
@@ -53,21 +58,40 @@ public:
 
 private:
 	MESSAGE_FUNC_PARAMS(OnMedigunInfoUpdate, "MedigunInfo", attributes);
+	MESSAGE_FUNC_PARAMS(OnReloadControlSettings, "ReloadControlSettings", data);
 
 	bool alive;
+	int charges;
 	float level;
 	TFMedigun medigun;
+	bool released;
 	TFResistType resistType;
 	TFTeam team;
 };
 
 MedigunInfo::MedigunInfo(std::string name) : Module(name) {
+	mainPanel = nullptr;
+
 	enabled = new ConVar("statusspec_mediguninfo_enabled", "0", FCVAR_NONE, "enable medigun info", [](IConVar *var, const char *pOldValue, float flOldValue) { g_ModuleManager->GetModule<MedigunInfo>("Medigun Info")->ToggleEnabled(var, pOldValue, flOldValue); });
 	reload_settings = new ConCommand("statusspec_mediguninfo_reload_settings", []() { g_ModuleManager->GetModule<MedigunInfo>("Medigun Info")->ReloadSettings(); }, "reload settings for the medigun info HUD from the resource file", FCVAR_NONE);
 }
 
 bool MedigunInfo::CheckDependencies(std::string name) {
 	bool ready = true;
+
+	if (!g_pVGuiSchemeManager) {
+		PRINT_TAG();
+		Warning("Required interface vgui::ISchemeManager for module %s not available!\n", name.c_str());
+
+		ready = false;
+	}
+
+	if (!g_pVGuiSurface) {
+		PRINT_TAG();
+		Warning("Required interface vgui::ISurface for module %s not available!\n", name.c_str());
+
+		ready = false;
+	}
 
 	if (!Interfaces::vguiLibrariesAvailable) {
 		PRINT_TAG();
@@ -137,11 +161,13 @@ void MedigunInfo::ToggleEnabled(IConVar *var, const char *pOldValue, float flOld
 				}
 				else {
 					Warning("Could not initialize the panel!\n");
+					mainPanel = nullptr;
 					var->SetValue(0);
 				}
 			}
 			catch (bad_pointer) {
 				Warning("Could not initialize the panel!\n");
+				mainPanel = nullptr;
 				var->SetValue(0);
 			}
 		}
@@ -181,13 +207,21 @@ void MedigunInfo::MainPanel::ApplySettings(KeyValues *inResourceData) {
 	ComputePos(inResourceData->GetString("blu_base_x"), bluBaseX, wide, alignScreenWide, true);
 	ComputePos(inResourceData->GetString("blu_base_y"), bluBaseY, tall, alignScreenTall, false);
 
-	redOffsetX = inResourceData->GetInt("red_offset_x");
-	redOffsetY = inResourceData->GetInt("red_offset_y");
-	bluOffsetX = inResourceData->GetInt("blu_offset_x");
-	bluOffsetY = inResourceData->GetInt("blu_offset_y");
+	redOffsetX = g_pVGuiSchemeManager->GetProportionalScaledValue(inResourceData->GetInt("red_offset_x"));
+	redOffsetY = g_pVGuiSchemeManager->GetProportionalScaledValue(inResourceData->GetInt("red_offset_y"));
+	bluOffsetX = g_pVGuiSchemeManager->GetProportionalScaledValue(inResourceData->GetInt("blu_offset_x"));
+	bluOffsetY = g_pVGuiSchemeManager->GetProportionalScaledValue(inResourceData->GetInt("blu_offset_y"));
+}
+
+void MedigunInfo::MainPanel::LoadControlSettings(const char *dialogResourceName, const char *pathID, KeyValues *pPreloadedKeyValues, KeyValues *pConditions) {
+	vgui::EditablePanel::LoadControlSettings(dialogResourceName, pathID, pPreloadedKeyValues, pConditions);
+
+	PostActionSignal(new KeyValues("ReloadControlSettings"));
 }
 
 void MedigunInfo::MainPanel::OnTick() {
+	vgui::EditablePanel::OnTick();
+
 	size_t bluMediguns = 0;
 	size_t redMediguns = 0;
 
@@ -198,32 +232,37 @@ void MedigunInfo::MainPanel::OnTick() {
 
 				if (weapon && Entities::CheckEntityBaseclass(weapon, "WeaponMedigun")) {
 					MedigunPanel *medigunPanel;
+					int x, y;
 
 					if (player.GetTeam() == TFTeam_Red) {
 						redMediguns++;
 
 						if (redMediguns > redMedigunPanels.size()) {
 							medigunPanel = new MedigunPanel(this, "MedigunPanel");
+							AddActionSignalTarget(medigunPanel);
 							redMedigunPanels.push_back(medigunPanel);
 						}
 						else {
 							medigunPanel = redMedigunPanels.at(redMediguns - 1);
 						}
 
-						medigunPanel->SetPos(redBaseX + (redOffsetX * (redMediguns - 1)), redBaseY + (redOffsetY * (redMediguns - 1)));
+						x = redBaseX + (redOffsetX * (redMediguns - 1));
+						y = redBaseY + (redOffsetY * (redMediguns - 1));
 					}
 					else if (player.GetTeam() == TFTeam_Blue) {
 						bluMediguns++;
 
 						if (bluMediguns > bluMedigunPanels.size()) {
 							medigunPanel = new MedigunPanel(this, "MedigunPanel");
+							AddActionSignalTarget(medigunPanel);
 							bluMedigunPanels.push_back(medigunPanel);
 						}
 						else {
 							medigunPanel = bluMedigunPanels.at(bluMediguns - 1);
 						}
 
-						medigunPanel->SetPos(bluBaseX + (bluOffsetX * (bluMediguns - 1)), bluBaseY + (bluOffsetY * (bluMediguns - 1)));
+						x = bluBaseX + (bluOffsetX * (bluMediguns - 1));
+						y = bluBaseY + (bluOffsetY * (bluMediguns - 1));
 					}
 
 					KeyValues *medigunInfo = new KeyValues("MedigunInfo");
@@ -243,13 +282,18 @@ void MedigunInfo::MainPanel::OnTick() {
 						type = TFMedigun_Vaccinator;
 					}
 
+					float level = *Entities::GetEntityProp<float *>(weapon, { "m_flChargeLevel" });
+
 					medigunInfo->SetBool("alive", player.IsAlive());
-					medigunInfo->SetFloat("level", *Entities::GetEntityProp<float *>(weapon, { "m_flChargeLevel" }));
+					medigunInfo->SetInt("charges", type == TFMedigun_Vaccinator ? int(floor(level * 4.0f)) : int(floor(level)));
+					medigunInfo->SetFloat("level", level);
 					medigunInfo->SetInt("medigun", type);
+					medigunInfo->SetBool("released", *Entities::GetEntityProp<bool *>(weapon, { "m_bChargeRelease" }));
 					medigunInfo->SetInt("resistType", *Entities::GetEntityProp<int *>(weapon, { "m_nChargeResistType" }));
 					medigunInfo->SetInt("team", player.GetTeam());
 
 					PostMessage(medigunPanel, medigunInfo);
+					medigunPanel->SetPos(x, y);
 
 					break;
 				}
@@ -271,20 +315,152 @@ void MedigunInfo::MainPanel::OnTick() {
 MedigunInfo::MedigunPanel::MedigunPanel(vgui::Panel *parent, const char *panelName) : vgui::EditablePanel(parent, panelName) {}
 
 void MedigunInfo::MedigunPanel::OnMedigunInfoUpdate(KeyValues *attributes) {
-	if (alive != attributes->GetBool("alive") || medigun != attributes->GetInt("medigun") || resistType != attributes->GetInt("resistType") || team != attributes->GetInt("team")) {
-		LoadControlSettings("Resource/UI/MedigunInfoSingle.res", nullptr, nullptr, attributes);
-	}
+	bool reloadSettings = (alive != attributes->GetBool("alive") || charges != attributes->GetInt("charges") || medigun != attributes->GetInt("medigun") || released != attributes->GetBool("released") || resistType != attributes->GetInt("resistType") || team != attributes->GetInt("team"));
 
 	alive = attributes->GetBool("alive");
-	level = attributes->GetFloat("percentage");
+	charges = attributes->GetInt("charges");
+	level = attributes->GetFloat("level");
 	medigun = (TFMedigun) attributes->GetInt("medigun");
+	released = attributes->GetBool("released");
 	resistType = (TFResistType) attributes->GetInt("resistType");
 	team = (TFTeam) attributes->GetInt("team");
 
-	SetDialogVariable("redcharge", int(floor(level * 100.0f)));
-	SetDialogVariable("redcharges", int(floor(level * 4.0f)));
-	SetDialogVariable("redcharge1", int(floor(level * 400.0f) - 0.0f) < 0 ? 0 : int(floor(level * 400.0f) - 0.0f));
-	SetDialogVariable("redcharge2", int(floor(level * 400.0f) - 100.0f) < 0 ? 0 : int(floor(level * 400.0f) - 100.0f));
-	SetDialogVariable("redcharge3", int(floor(level * 400.0f) - 200.0f) < 0 ? 0 : int(floor(level * 400.0f) - 200.0f));
-	SetDialogVariable("redcharge4", int(floor(level * 400.0f) - 300.0f) < 0 ? 0 : int(floor(level * 400.0f) - 300.0f));
+	if (reloadSettings) {
+		KeyValues *conditions = new KeyValues("conditions");
+
+		if (alive) {
+			conditions->SetBool("player-alive", true);
+		}
+		else {
+			conditions->SetBool("player-dead", true);
+		}
+
+		if (charges == 0) {
+			conditions->SetBool("charges-0", true);
+		}
+		else if (charges == 1) {
+			conditions->SetBool("charges-1", true);
+		}
+		else if (charges == 2) {
+			conditions->SetBool("charges-2", true);
+		}
+		else if (charges == 3) {
+			conditions->SetBool("charges-3", true);
+		}
+		else if (charges == 4) {
+			conditions->SetBool("charges-4", true);
+		}
+
+		if (medigun == TFMedigun_MediGun) {
+			conditions->SetBool("medigun-medigun", true);
+		}
+		else if (medigun == TFMedigun_Kritzkrieg) {
+			conditions->SetBool("medigun-kritzkrieg", true);
+		}
+		else if (medigun == TFMedigun_QuickFix) {
+			conditions->SetBool("medigun-quickfix", true);
+		}
+		else if (medigun == TFMedigun_Vaccinator) {
+			conditions->SetBool("medigun-vaccinator", true);
+		}
+
+		if (released) {
+			conditions->SetBool("status-released", true);
+		}
+		else {
+			conditions->SetBool("status-building", true);
+		}
+
+		if (resistType == TFResistType_Bullet) {
+			conditions->SetBool("resist-bullet", true);
+		}
+		else if (resistType == TFResistType_Explosive) {
+			conditions->SetBool("resist-explosive", true);
+		}
+		else if (resistType == TFResistType_Fire) {
+			conditions->SetBool("resist-fire", true);
+		}
+
+		if (team == TFTeam_Red) {
+			conditions->SetBool("team-red", true);
+		}
+		else if (team == TFTeam_Blue) {
+			conditions->SetBool("team-blu", true);
+		}
+
+		LoadControlSettings("Resource/UI/MedigunPanel.res", nullptr, nullptr, conditions);
+	}
+
+	SetDialogVariable("charge", int(floor(level * 100.0f)));
+	SetDialogVariable("charges", int(floor(level * 4.0f)));
+	SetDialogVariable("charge1", int(floor(level * 400.0f) - 0.0f) < 0 ? 0 : int(floor(level * 400.0f) - 0.0f));
+	SetDialogVariable("charge2", int(floor(level * 400.0f) - 100.0f) < 0 ? 0 : int(floor(level * 400.0f) - 100.0f));
+	SetDialogVariable("charge3", int(floor(level * 400.0f) - 200.0f) < 0 ? 0 : int(floor(level * 400.0f) - 200.0f));
+	SetDialogVariable("charge4", int(floor(level * 400.0f) - 300.0f) < 0 ? 0 : int(floor(level * 400.0f) - 300.0f));
+}
+
+void MedigunInfo::MedigunPanel::OnReloadControlSettings(KeyValues *attributes) {
+	KeyValues *conditions = new KeyValues("conditions");
+
+	if (alive) {
+		conditions->SetBool("player-alive", true);
+	}
+	else {
+		conditions->SetBool("player-dead", true);
+	}
+
+	if (charges == 0) {
+		conditions->SetBool("charges-0", true);
+	}
+	else if (charges == 1) {
+		conditions->SetBool("charges-1", true);
+	}
+	else if (charges == 2) {
+		conditions->SetBool("charges-2", true);
+	}
+	else if (charges == 3) {
+		conditions->SetBool("charges-3", true);
+	}
+	else if (charges == 4) {
+		conditions->SetBool("charges-4", true);
+	}
+
+	if (medigun == TFMedigun_MediGun) {
+		conditions->SetBool("medigun-medigun", true);
+	}
+	else if (medigun == TFMedigun_Kritzkrieg) {
+		conditions->SetBool("medigun-kritzkrieg", true);
+	}
+	else if (medigun == TFMedigun_QuickFix) {
+		conditions->SetBool("medigun-quickfix", true);
+	}
+	else if (medigun == TFMedigun_Vaccinator) {
+		conditions->SetBool("medigun-vaccinator", true);
+	}
+
+	if (released) {
+		conditions->SetBool("status-released", true);
+	}
+	else {
+		conditions->SetBool("status-building", true);
+	}
+
+	if (resistType == TFResistType_Bullet) {
+		conditions->SetBool("resist-bullet", true);
+	}
+	else if (resistType == TFResistType_Explosive) {
+		conditions->SetBool("resist-explosive", true);
+	}
+	else if (resistType == TFResistType_Fire) {
+		conditions->SetBool("resist-fire", true);
+	}
+
+	if (team == TFTeam_Red) {
+		conditions->SetBool("team-red", true);
+	}
+	else if (team == TFTeam_Blue) {
+		conditions->SetBool("team-blu", true);
+	}
+
+	LoadControlSettings("Resource/UI/MedigunPanel.res", nullptr, nullptr, conditions);
 }
